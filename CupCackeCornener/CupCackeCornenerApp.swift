e6611746e08cfb4bc41c44b829b2c9d7b8c0bd3e
Christopher Peralta <114698519+ChristopherPeralta@users.noreply.github.com>
2024-03-06 09:08:12 -0500

    init

---
+++ CupCackeCornener/CupCackeCornenerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupCackeCornenerApp.swift
+//  CupCackeCornener
+//
+//  Created by Christopher Peralta on 5/02/24.
+//
+
+import SwiftUI
+
+@main
+struct CupCackeCornenerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
